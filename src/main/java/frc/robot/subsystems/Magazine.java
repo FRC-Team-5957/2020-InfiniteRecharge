89dce47a51ec6cdda76ef4c7cc80d9be6ba5b408
Kelvin Kariuki <44537466+KelvinAstro@users.noreply.github.com>
2020-02-27 16:44:40 -0500

    added magazine command

--- src/main/java/frc/robot/subsystems/Magazine.java
+++ src/main/java/frc/robot/subsystems/Magazine.java
@@ -1,27 +1,27 @@
-// package frc.robot.subsystems;
+package frc.robot.subsystems;
 
-// import frc.robot.Constants;
-// import frc.robot.RobotMap;
+import frc.robot.Constants;
+import frc.robot.RobotMap;
 
-// import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-// import edu.wpi.first.wpilibj2.command.SubsystemBase;
-// import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.ControlMode;
 
-// public class Magazine extends SubsystemBase {
-//     TalonSRX magBot, magTop;
+public class Magazine extends SubsystemBase {
+    TalonSRX magBot, magTop;
 
-//     public Magazine() {
-//         initMotors();
-//     }
+    public Magazine() {
+        initMotors();
+    }
 
-//     public void initMotors() {
-//         magBot = new TalonSRX(RobotMap.MAGBOT_BELT);
-//         magTop = new TalonSRX(RobotMap.MAGTOP_BELT);
-//     }
+    public void initMotors() {
+        magBot = new TalonSRX(RobotMap.MAGBOT_BELT);
+        magTop = new TalonSRX(RobotMap.MAGTOP_BELT);
+    }
 
-//     public void advance() {
-//         magBot.set(ControlMode.PercentOutput, Constants.INTAKE_SPEED);
-//         magTop.set(ControlMode.PercentOutput, Constants.INTAKE_SPEED);
-//     }
-//     //Add in pneumatic pistons 
-// }
+    public void advance() {
+        magBot.set(ControlMode.PercentOutput, Constants.INTAKE_SPEED);
+        magTop.set(ControlMode.PercentOutput, Constants.INTAKE_SPEED);
+    }
+    //Add in pneumatic pistons 
+}
