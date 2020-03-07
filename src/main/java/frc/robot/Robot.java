ba9358aff6ccc384a61757fb9bc3a1adbea56376
Kelvin Kariuki <44537466+KelvinAstro@users.noreply.github.com>
2020-03-06 21:08:13 -0500

    test

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 import java.io.IOException;
 
@@ -93,6 +94,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotPeriodic() {
+    CommandScheduler.getInstance().run();
   }
 
   /**
